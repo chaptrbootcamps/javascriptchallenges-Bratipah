c2e9f4724cb406c55181d6b1d5dd3a0194990ac4
bratipah <bratipah@gmail.com>
2021-08-09 11:44:41 +0300

    add area,date

---
+++ script.js
@@ -0,0 +1,22 @@
+// Area of a traingle 
+var side1 = 5
+var side2 = 6
+var side3 = 7
+var height = (side1 + side2 + side3)/2
+// console.log(height)
+ var area = Math.sqrt(height * ((height-side1) * (height -side2) * (height - side3)))
+ console.log(area)
+
+
+// Convert temperatures to and from celsius, fahrenheit
+
+const celsius = prompt("Enter a celsius value: ");
+const fahrenheit = (celsius * 1.8) + 32
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
+
+
+// current date
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+document.getElementById("dt").innerHTML = date;
